5c3cf91c65c90ef1a909e0a5cb45e678e43a030a
Smirnov Dmitriy <d.smirnov2006@mail.ru>
2026-08-20 01:05:56 +0300

    add upds: q. ? add upd

--- src/http/service/TicketService.java
+++ src/http/service/TicketService.java
@@ -1,18 +1,24 @@
 package service;
 
 
+import dao.TicketDao;
 import dto.TicketDto;
 
 import java.util.List;
 
 public class TicketService {
 
-    public static final TicketService INSTANCE = new TicketService();
+    private static final TicketService INSTANCE = new TicketService();
+
+    private final TicketDao ticketDao = TicketDao.getInstance();
 
     private TicketService() {
     }
 
-    public List<TicketDto>
+    public List<TicketDto> findAllByFlightId(Long flightId){
+        return ticketDao.findAllByFlightId(flightId).stream()
+                .map(List)
+    }
 
     public static TicketService getInstance(){
         return INSTANCE;
