ad697bb106ed5b2dcd522d9b9931e4e04fc47a98
Smirnov Dmitriy <d.smirnov2006@mail.ru>
2026-08-20 17:07:18 +0300

    add method

--- src/http/service/TicketService.java
+++ src/http/service/TicketService.java
@@ -6,6 +6,8 @@ import dto.TicketDto;
 
 import java.util.List;
 
+import static java.util.stream.Collectors.toList;
+
 public class TicketService {
 
     private static final TicketService INSTANCE = new TicketService();
@@ -17,7 +19,12 @@ public class TicketService {
 
     public List<TicketDto> findAllByFlightId(Long flightId){
         return ticketDao.findAllByFlightId(flightId).stream()
-                .map(List)
+                .map(ticket -> new TicketDto(
+                        ticket.getId(),
+                        ticket.getFlightId(),
+                        ticket.getSeatNo()
+                ))
+                .collect(toList());
     }
 
     public static TicketService getInstance(){
