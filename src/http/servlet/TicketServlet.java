8fead056be4fb489ff56df6a86a3879ca71fb051
Smirnov Dmitriy <d.smirnov2006@mail.ru>
2026-08-21 18:40:55 +0300

    add instance

--- src/http/servlet/TicketServlet.java
+++ src/http/servlet/TicketServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import service.TicketService;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +14,8 @@ import java.io.PrintWriter;
 @WebServlet("/tickets")
     public class TicketServlet extends HttpServlet{
 
+    private final TicketService ticketService = TicketService.getInstance();
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -20,6 +23,9 @@ import java.io.PrintWriter;
 
         try (var printWriter = resp.getWriter()) {
             printWriter.write("<h1>Купленные билеты:</h1>");
+            printWriter.write("<ul>");
+
+            printWriter.write("</ul>");
         }
     }
 }
