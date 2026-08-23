e4300d74442ef426ba4411d02d8b6daffaa4f304
Smirnov Dmitriy <d.smirnov2006@mail.ru>
2026-08-23 18:57:52 +0300

    add utf

--- src/http/servlet/TicketServlet.java
+++ src/http/servlet/TicketServlet.java
@@ -9,6 +9,7 @@ import service.TicketService;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 
 @WebServlet("/tickets")
@@ -21,10 +22,18 @@ import java.io.PrintWriter;
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         var flightId = Long.valueOf(req.getParameter("flightId"));
 
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
         try (var printWriter = resp.getWriter()) {
             printWriter.write("<h1>Купленные билеты:</h1>");
             printWriter.write("<ul>");
-
+            ticketService.findAllByFlightId(flightId).forEach(
+                    ticketDto -> printWriter.write("""
+                            <li>
+                            %s
+                            </li>
+                            """.formatted(ticketDto.getSeatNo()))
+            );
             printWriter.write("</ul>");
         }
     }
